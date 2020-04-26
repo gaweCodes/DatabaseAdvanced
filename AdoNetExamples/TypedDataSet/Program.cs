08b35db89c49bd35c7adc5a1837c0797874c4097
Gabriel Weibel <gabriel.weibel@hotmail.de>
2020-04-26 19:43:29 +0200

    typed dataset

---
+++ AdoNetExamples/TypedDataSet/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using TypedDataSet.NorthwindDataSetTableAdapters;
+
+namespace TypedDataSet
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var dataSet = new NorthwindDataSet();
+            var tblAdapter = new ProductsTableAdapter();
+            tblAdapter.Fill(dataSet.Products);
+
+            var tblAdapterCategories = new CategoriesTableAdapter();
+            tblAdapterCategories.Fill(dataSet.Categories);
+
+            foreach (var row in dataSet.Products)
+                Console.WriteLine($"{row.ProductName}");
+            var peterProduct = dataSet.Products.FirstOrDefault(x => x.ProductName == "Peter");
+            var gabrielProduct = dataSet.Products.FirstOrDefault(x => x.ProductName == "Gabriel");
+            if (gabrielProduct == default(NorthwindDataSet.ProductsRow) && peterProduct == default(NorthwindDataSet.ProductsRow))
+            {
+                var newRow = dataSet.Products.NewProductsRow();
+                newRow.ProductName = "Gabriel";
+                newRow.Discontinued = false;
+                newRow.CategoriesRow = dataSet.Categories.First();
+                dataSet.Products.AddProductsRow(newRow);
+            }
+            else if(peterProduct == default(NorthwindDataSet.ProductsRow))
+            {
+                gabrielProduct.BeginEdit();
+                gabrielProduct.ProductName = "Peter";
+                gabrielProduct.EndEdit();
+            }
+            else if (gabrielProduct == default(NorthwindDataSet.ProductsRow))
+                peterProduct.Delete();
+            tblAdapter.Update(dataSet);
+            Console.ReadLine();
+        }
+    }
+}
